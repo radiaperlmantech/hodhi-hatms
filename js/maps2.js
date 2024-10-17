6a596b0180f3302b3eb8552557bfa138d78074eb
Silas Mungiria <mungiria8@gmail.com>
2024-10-17 03:19:37 +0300

    Update contact information and Google Maps location

--- js/maps2.js
+++ js/maps2.js
@@ -1,19 +1,15 @@
 $(document).on("ready", function () {
-  //------- Google Maps ---------//
+  var newYorkLatLng = new google.maps.LatLng(40.7222, -74.0009);
+  var nairobiLatLng = new google.maps.LatLng(-1.258, 36.8219);
 
-  // Creating a LatLng object containing the coordinate for the center of the map
-  var latlng = new google.maps.LatLng(53.385846, -1.471385);
-
-  // Creating an object literal containing the properties we want to pass to the map
   var options = {
-    zoom: 15, // This number can be set to define the initial zoom level of the map
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP, // This value can be set to define the map type ROADMAP/SATELLITE/HYBRID/TERRAIN
+    zoom: 12,
+    center: new google.maps.LatLng(-1.258, 36.8219),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
-  // Calling the constructor, thereby initializing the map
+
   var map = new google.maps.Map(document.getElementById("map_div"), options);
 
-  // Define Marker properties
   var image = new google.maps.MarkerImage(
     "images/marker1.png",
     new google.maps.Size(100, 100),
@@ -21,10 +17,17 @@ $(document).on("ready", function () {
     new google.maps.Point(18, 42)
   );
 
-  // Add Marker
-  var marker1 = new google.maps.Marker({
-    position: new google.maps.LatLng(53.385846, -1.471385),
+  var markerNewYork = new google.maps.Marker({
+    position: newYorkLatLng,
     map: map,
-    icon: image, // This path is the custom pin to be shown. Remove this line and the proceeding comma to use default pin
+    title: "Hodhi Inc, New York",
+    icon: image,
+  });
+
+  var markerNairobi = new google.maps.Marker({
+    position: nairobiLatLng,
+    map: map,
+    title: "Hodhi Inc, Nairobi",
+    icon: image,
   });
 });
